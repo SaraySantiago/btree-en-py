a0c10c471b482eaf9bef093bf1b3630c7ec2ded1
SaraySantiago <ssantiago18@iesrioarba.es>
2024-10-18 14:16:09 +0200

    .

--- bnodo.py
+++ bnodo.py
@@ -1,4 +1,4 @@
-
+#C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
 class bnodo: 
     __info = None
     __de = None
@@ -7,5 +7,16 @@ class bnodo:
 def __init__(self,info = None ) -> None: 
     self.__info = info
 
+def __str__(self) -> str:
+    answ = "("
+    if self.__iz == None:
+        answ += "(.)"
+    if self.__de == None:
+        answ += "(.)"
+    answ += str (self.__info) 
+    answ += ")"
+    return answ 
+
 if __name__ == "__main__":
     bn0 = bnodo ()
+    print (bn0)
