77ba89a3dc090b8cc7b6392f5c8dc5741cd52333
SaraySantiago <ssantiago18@iesrioarba.es>
2024-10-18 14:03:50 +0200

    .

---
+++ bnodo.py
@@ -0,0 +1,11 @@
+
+class bnodo: 
+    __info = None
+    __de = None
+    __iz = None
+
+def __init__(self,info = None ) -> None: 
+    self.__info = info
+
+if __name__ == "__main__":
+    bn0 = bnodo ()
